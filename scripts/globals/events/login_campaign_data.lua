8b2ab8c52aee76bff800039ff241fb93925af041
kaincenteno <kain.centeno@catsangel.com>
2020-11-19 00:51:03 -0800

    thank you Deridjian from Canaria server for table for NOV2020

--- scripts/globals/events/login_campaign_data.lua
+++ scripts/globals/events/login_campaign_data.lua
@@ -175,4 +175,139 @@ tpz.events.loginCampaign.prizes =
             },
         },
     },
+    [2020] =
+    {
+        [11] = -- November 2020
+        {
+            [1] =
+            {
+                ["price"] = 10,
+                ["items"] =
+                {
+                     1126, -- Beastmen's Seal
+                     1127, -- Kindred's Seal
+                     2955, -- Kindred's Crest
+                     2956, -- High Kindred's Crest
+                     2957, -- Sacred Kindred's Crest
+                     1857, -- Cordial Invite
+                     2306, -- Martial Ball Invite
+                     5364, -- Training Grounds Key
+                     2487, -- Mercenary Camp Entry Slip
+                     5741, -- Flask Of Pest Repellent
+                     3557, -- Athena Orb
+                     5113, -- Cracked Nut
+                     3541, -- Seasoning Stone
+                     3543, -- Fossilized Fang
+                     3542, -- Fossilized Bone
+                     5724, -- Pungent Powder
+                     6535, -- Pungent Powder II
+                     9890, -- Tarazacum Orb
+                },
+            },
+
+            [5] =
+            {
+                ["price"] = 100,
+                ["items"] =
+                {
+                     8734, -- Mog Kupon I-S1
+                     8966, -- Eudaemon Blade
+                     8967, -- Eudaemon Cape
+                     8968, -- Eudaemon Ring
+                     8969, -- Eudaemon Sash
+                     8970, -- Eudaemon Shield
+                    17006, -- Drill Calamary
+                    17007, -- Dwarf Pugil
+                     6413, -- Astral Cube
+                     9891, -- Zinnia Orb
+                    20713, -- Excalipoor
+                     6008, -- Piece of Copse Candy
+                },
+            },
+
+            [9] =
+            {
+                ["price"] = 300,
+                ["items"] =
+                {
+                    10051, -- ♪Crab
+                    10058, -- ♪Beetle
+                    10069, -- ♪Goobbue
+                    10073, -- ♪Dhalmel
+                    10384, -- Cumulus Masque
+                    20666, -- Blizzard Brand
+                    25658, -- Wyrm. Masque +1
+                    25757, -- Wyrmking Suit +1
+                },
+            },
+
+            [13] =
+            {
+                ["price"] = 500,
+                ["items"] =
+                {
+                    10076, -- ♪Golden Bomb
+                },
+
+            },
+
+            [17] =
+            {
+                ["price"] = 750,
+                ["items"] =
+                {
+                     9079, -- Kitchen Brick
+                     9080, -- Kitchen Stove
+                     9081, -- Kitchen Plate
+                     3339, -- Honey Wine
+                     3341, -- Beastly Shank
+                     3343, -- Blue Pondweed
+                     1873, -- Brigand's Chart
+                     1874, -- Pirate's Chart
+                },
+            },
+
+            [21] =
+            {
+                ["price"] = 1000,
+                ["items"] =
+                {
+                     6499, -- Patio Design Plans
+                    26164, -- Caliber Ring
+                    26165, -- Facility Ring
+                     6486, -- Frayed Sack (Pel)
+                     6487, -- Frayed Sack (Fer)
+                     6488, -- Frayed Sack (Tau)
+                     6542, -- Worn Sack (SS+2)
+                     6544, -- Worn Sack (LS+2)
+                     6546, -- Worn Sack (DS+2)
+                     6548, -- Worn Sack (ST+2)
+                     6550, -- Worn Sack (LT+2)
+                     6552, -- Worn Sack (DT+2)
+                     6554, -- Worn Sack (SD+2)
+                     6556, -- Worn Sack (LD+2)
+                     6558, -- Worn Sack (DD+2)
+                     6560, -- Worn Sack (SO+2)
+                     6562, -- Worn Sack (LO+2)
+                     6564, -- Worn Sack (DO+2)
+                },
+            },
+
+            [25] =
+            {
+                ["price"] = 1500,
+                ["items"] =
+                {
+                     3340, -- Sweet Tea
+                     3342, -- Savory Shank
+                     3344, -- Red Pondweed
+                     8720, -- Maliya. Coral Orb
+                     8722, -- Hepatizon Ingot
+                     8724, -- Beryllium Ingot
+                     8726, -- Exalted Lumber
+                     8728, -- Sif's Macrame
+                },
+            },
+        },
+    },
 }
